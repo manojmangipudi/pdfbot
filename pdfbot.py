423b266ead794609d66cb764dea8bbdeb0b42d31
manojmangipudi <manojmangipudi410@gmail.com>
2024-05-16 12:26:48 +0200

    new file:   .gitignore 	new file:   .python-version 	modified:   pdfbot.py

--- pdfbot.py
+++ pdfbot.py
@@ -1,1 +1,14 @@
-import argparse
+import streamlit as st
+
+
+def main():
+    st.set_page_config(page_title= "chat with multiple PDF's", page_icon= ":books:")
+    st.header("Chat with multiple PDFs : books:")
+    st.text_input("Ask a question about your documents:")
+    with st.sidebar:
+        st.subheader("Your documents")
+        st.file_uploader("Upload your PDF's here and click on 'Process'")
+        st.button("Process")
+
+if __name__ == '__main__':
+    main()
